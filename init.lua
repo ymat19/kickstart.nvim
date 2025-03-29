82c1244f6c907895aa3032629bcf7078f4d301af
ymat19 <ymat19>
2025-03-30 05:59:01 +0900

    feat: enable git sign

--- init.lua
+++ init.lua
@@ -265,7 +265,6 @@ require('lazy').setup({
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
-    enabled = false,
     opts = {
       signs = {
         add = { text = '+' },
