1eb9592cdc29406f398f6d027f60f8ac1f02ec99
ymat19 <ymat19>
2025-04-01 06:19:10 +0900

    feat: add snacks terminal settings

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -37,6 +37,50 @@ return {
       notification = {
         -- wo = { wrap = true } -- Wrap notifications
       },
+      terminal = {
+        bo = {
+          filetype = 'snacks_terminal',
+        },
+        wo = {},
+        keys = {
+          q = 'hide',
+          gf = function(self)
+            local f = vim.fn.findfile(vim.fn.expand '<cfile>', '**')
+            if f == '' then
+              Snacks.notify.warn 'No file under cursor'
+            else
+              self:hide()
+              vim.schedule(function()
+                vim.cmd('e ' .. f)
+              end)
+            end
+          end,
+          term_normal = {
+            '<esc>',
+            function(self)
+              self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
+              if self.esc_timer:is_active() then
+                self.esc_timer:stop()
+                vim.cmd 'stopinsert'
+              else
+                self.esc_timer:start(200, 0, function() end)
+                return '<esc>'
+              end
+            end,
+            mode = 't',
+            expr = true,
+            desc = 'Double escape to normal mode',
+          },
+          term_hide = {
+            '<c-t>',
+            function(self)
+              self:hide()
+            end,
+            mode = 't',
+            desc = 'Hide terminal with <C-t>',
+          },
+        },
+      },
     },
   },
   keys = {
@@ -509,9 +553,9 @@ return {
       desc = 'Toggle Terminal',
     },
     {
-      '<c-_>',
+      '<c-t>',
       function()
-        Snacks.terminal()
+        Snacks.terminal 'zsh'
       end,
       desc = 'which_key_ignore',
     },
